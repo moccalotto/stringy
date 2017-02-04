c670b48989e8d6efb2d0d6635adf6a4f98a5ff66
Kim Ravn Hansen <khansen@Kims-MacBook-Pro.local>
2017-02-04 21:17:07 +0100

    More testing.

--- spec/Moccalotto/Stringy/StringySpec.php
+++ spec/Moccalotto/Stringy/StringySpec.php
@@ -1,21 +1,52 @@
 <?php
 
+/**
+ * Stringy test package.
+ *
+ * @codingStandardsIgnoreFile
+ */
 namespace spec\Moccalotto\Stringy;
 
+use Moccalotto\Stringy\Stringy;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
 class StringySpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    function it_is_initializable_as_an_empty_utf8_string()
     {
-        $this->shouldHaveType('Moccalotto\Stringy\Stringy');
+        $this->shouldHaveType(Stringy::class);
     }
 
-    function it_can_make_a_string_shorter()
+    function it_has_sane_defaults_with_empty_constructor()
+    {
+        $this->string()->shouldBe('');
+        $this->encoding()->shouldBe('UTF-8');
+    }
+
+    function it_contains_a_string()
+    {
+        $this->beConstructedWith('foo');
+        $this->string()->shouldBe('foo');
+    }
+
+    function it_contains_an_encoding()
+    {
+        $this->beConstructedWith('foo', 'ASCII');
+        $this->encoding()->shouldBe('ASCII');
+    }
+
+    function it_has_a_static_constructor()
+    {
+        $this->beConstructedThrough('create', ['foo', 'Windows-1252']);
+        $this->encoding()->shouldBe('Windows-1252');
+        $this->string()->shouldBe('foo');
+    }
+
+    function it_can_make_a_string_shorter_via_the_limit_method()
     {
         $this->beConstructedWith('test string');
-        $this->limit(4)->shouldHaveType('Moccalotto\Stringy\Stringy');
+        $this->limit(0)->shouldHaveType(Stringy::class);
         $this->limit(4)->string()->shouldBe('test');
     }
 }
