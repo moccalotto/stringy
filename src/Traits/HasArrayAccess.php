173ce659a3d0c99cd57bd5dfc1117bf1adea1141
Kim Ravn Hansen <khansen@Kims-MacBook-Pro.local>
2017-02-07 09:57:24 +0100

    Refactor.

--- src/Traits/HasArrayAccess.php
+++ src/Traits/HasArrayAccess.php
@@ -5,8 +5,30 @@ namespace Moccalotto\Stringy\Traits;
 use OutOfRangeException;
 use OutOfBoundsException;
 
+/**
+ * Adds array access to Stringy
+ */
 trait HasArrayAccess
 {
+    protected function ensureOffsetOk($offset)
+    {
+        if ($offset != intval($offset)) {
+            throw new OutOfBoundsException('Invalid non-integer offset');
+        }
+
+        if ($offset < 0) {
+            throw new OutOfRangeException('Offset must be >= 0');
+        }
+
+        if ($offset >= $this->length()) {
+            throw new OutOfRangeException(sprintf(
+                'Illegal offset "%d". Must be lower than length of string: %d',
+                $offset,
+                $this->length()
+            ));
+        }
+    }
+
     /**
      * @SuppressWarnings(PHPMD.UnusedFormalParameters)
      */
@@ -44,22 +66,7 @@ trait HasArrayAccess
 
     public function offsetGet($offset)
     {
-        if ($offset != intval($offset)) {
-            throw new OutOfBoundsException('Invalid non-integer offset');
-        }
-
-        if ($offset < 0) {
-            throw new OutOfRangeException('Offset must be >= 0');
-        }
-
-        if ($offset >= $this->length()) {
-            throw new OutOfRangeException(sprintf(
-                'Illegal offset "%d". Must be lower than length of string: %d',
-                $offset,
-                $this->length()
-            ));
-        }
-
+        $this->ensureOffsetOk($offset);
         return $this->characters()[(int) $offset];
     }
 }
