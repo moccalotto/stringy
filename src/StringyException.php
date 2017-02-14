1959da2f713e6c0b1b814967c30053fbb0c469e8
Kim Ravn Hansen <khansen@Kims-MacBook-Pro.local>
2017-02-14 07:39:18 +0100

    Docblocks.

--- src/StringyException.php
+++ src/StringyException.php
@@ -1,23 +1,44 @@
 <?php
 
+/*
+ * This file is part of the Stringy package.
+ *
+ * @package Stringy
+ * @author Kim Ravn Hansen <moccalotto@gmail.com>
+ * @copyright 2017
+ * @license MIT
+ */
+
 namespace Moccalotto\Stringy;
 
 use Exception;
 
+/**
+ * Exception during a Stringy operation.
+ */
 class StringyException extends Exception
 {
     /**
+     * The original content string.
+     *
      * @var string
      */
     protected $string;
 
     /**
+     * The original enconding.
+     *
      * @var string
      */
     protected $encoding;
 
     /**
-     * Constructor
+     * Constructor.
+     *
+     * @param string    $message  The message of the exception
+     * @param string    $string   The content string
+     * @param string    $encoding The attempted target encoding
+     * @param Exception $previous Any previous exception that might have triggered $this
      */
     public function __construct(string $message, string $string, string $encoding = 'UTF-8', Exception $previous = null)
     {
