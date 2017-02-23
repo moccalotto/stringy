e4a7f28b7ba278f5cf8e2274598a479b9501c194
Kim Ravn Hansen <krh@krh-workstation.local>
2017-02-23 12:45:27 +0100

    Reorder.

--- src/Stringy.php
+++ src/Stringy.php
@@ -390,6 +390,35 @@ class Stringy implements ArrayAccess, Countable, Serializable, JsonSerializable
     }
 
     /**
+     * Return the text that comes after $start and before $stop.
+     *
+     * @param Stringy|string $start
+     * @param Stringy|string $stop
+     * @param int            $pairIndex search for the nth pair and
+     *                                  find what lies between those strings
+     *
+     * @return Stringy the string between $start and $stop
+     */
+    public function between($start, $stop, int $pairIndex = 0)
+    {
+        $idxStart = $this->positionOf($start, $pairIndex);
+        $idxStop = $this->positionOf($stop, $pairIndex);
+
+        if ($idxStart === null || $idxStop === null) {
+            return static::create('');
+        }
+
+        $pos = $idxStart + static::create($start)->length();
+        $length = $idxStop - $pos;
+
+        if ($length <= 0) {
+            return static::create('');
+        }
+
+        return $this->substring($pos, $length);
+    }
+
+    /**
      * Remove the substring that comes after the nth $needle.
      *
      * @param Stringy|string $needle
@@ -421,35 +450,6 @@ class Stringy implements ArrayAccess, Countable, Serializable, JsonSerializable
         }
 
         return $this->after($needle, $index);
-    }
-
-    /**
-     * Return the text that comes after $start and before $stop.
-     *
-     * @param Stringy|string $start
-     * @param Stringy|string $stop
-     * @param int            $pairIndex search for the nth pair and
-     *                                  find what lies between those strings
-     *
-     * @return Stringy the string between $start and $stop
-     */
-    public function between($start, $stop, int $pairIndex = 0)
-    {
-        $idxStart = $this->positionOf($start, $pairIndex);
-        $idxStop = $this->positionOf($stop, $pairIndex);
-
-        if ($idxStart === null || $idxStop === null) {
-            return static::create('');
-        }
-
-        $pos = $idxStart + static::create($start)->length();
-        $length = $idxStop - $pos;
-
-        if ($length <= 0) {
-            return static::create('');
-        }
-
-        return $this->substring($pos, $length);
     }
 
     /**
